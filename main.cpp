d062b7452e6e5e44deaa8605c4064b4db50bdb39
He <314373866@qq.com>
2022-03-11 18:03:55 +0800

    通过兰伯特光照看人物三角形

--- main.cpp
+++ main.cpp
@@ -92,6 +92,7 @@ void triangle(Vec2i* pts, TGAImage& image, TGAColor color)
 void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color)
 {
     // sort the vertices, t0, t1, t2 lower−to−upper (bubblesort yay!) 
+    if (t0.y == t1.y && t0.y == t2.y) return; // i dont care about degenerate triangles
     if (t0.y > t1.y)std::swap(t0, t1);
     if (t0.y > t2.y) std::swap(t0, t2);
     if (t1.y > t2.y) std::swap(t1, t2);
@@ -126,13 +127,6 @@ void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color)
 
 int main(int argc, char** argv) 
 {
-    TGAImage frame(200, 200, TGAImage::RGB);
-    Vec2i pts[3] = { Vec2i(10,10), Vec2i(100, 30), Vec2i(190, 160) };
-    triangle(pts, frame, TGAColor(255, 0, 0, 255));
-    frame.flip_vertically(); // to place the origin in the bottom left corner of the image 
-    frame.write_tga_file("output.tga");
-    return 0;
-
     if (2 == argc)
     {
         model = new Model(argv[1]);
@@ -142,15 +136,26 @@ int main(int argc, char** argv)
         model = new Model("obj/african_head.obj");
     }
     TGAImage image(width, height, TGAImage::RGB);
-
-    Vec2i t0[3] = { Vec2i(10,70),Vec2i(50, 160),  Vec2i(70, 80) };
-    Vec2i t1[3] = { Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180) };
-    Vec2i t2[3] = { Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180) };
-
-    triangle(t0[0], t0[1], t0[2], image, red);
-    triangle(t1[0], t1[1], t1[2], image, white);
-    triangle(t2[0], t2[1], t2[2], image, green);
+    Vec3f light_dir(0, 0, -1);
+    for (int i = 0; i < model->nfaces(); i++)
+    {
+        std::vector<int> face = model->face(i);
+        Vec2i screen_coords[3];
+        Vec3f world_coords[3];
+        for (int j = 0; j < 3; j++)
+        {
+            Vec3f v = model->vert(face[j]);
+            screen_coords[j] = Vec2i((v.x + 1.) * width / 2., (v.y + 1.) * height / 2.);
+            world_coords[j] = v;
+        }
+        Vec3f normal = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);
+        normal.normalize();
+        float intensity = normal * light_dir;
+        if (intensity > 0)
+            triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+    }
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
+    delete model;
     return 0;
 }
