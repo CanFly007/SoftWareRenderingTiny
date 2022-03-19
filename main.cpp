848ba568d70a700a9e67d7ca356c7292dbebef8a
MrHe <314373866@qq.com>
2022-03-19 19:03:14 +0800

    增加Vec3的重心坐标计算，有zbuffer检测的triangle计算

--- main.cpp
+++ main.cpp
@@ -61,7 +61,32 @@ Vec3f barycentric(Vec2i* pts, Vec2i P)
         return Vec3f(-1, -1, -1);
     return Vec3f(u, v, w);
 }
+Vec3f barycentric(Vec3f* pts, Vec3f P)
+{
+    //因为三维空间的面积不能用叉积计算，两个Vec3叉积还是Vec3
+    //换用作者给出的
+    //https://github.com/ssloy/tinyrenderer/wiki/Lesson-2:-Triangle-rasterization-and-back-face-culling
+    Vec3f A = pts[0];
+    Vec3f B = pts[1];
+    Vec3f C = pts[2];
 
+    Vec3f s[2];
+    s[0] = Vec3f(C.x - A.x, B.x - A.x, A.x - P.x);
+    s[1] = Vec3f(C.y - A.y, B.y - A.y, A.y - P.y);
+    //for (int i = 2; i--; ) 
+    //{
+    //    s[i].x = C[i] - A[i];
+    //    s[i].y = B[i] - A[i];
+    //    s[i].z = A[i] - P[i];
+    //}
+    Vec3f u = s[0] ^ s[1];
+    if (std::abs(u.z) > 1e-2) // dont forget that u[2] is integer. If it is zero then triangle ABC is degenerate
+        return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);//因为u的结果是(u,v,1)
+    return Vec3f(-1, 1, 1); // in this case generate negative coordinates, it will be thrown away by the rasterizator
+}
+
+//包围盒，重心坐标返回负值表示不在三角形内部不绘制
+//Vec2i pts[3] = { Vec2i(10,10), Vec2i(100, 30), Vec2i(190, 160) }; pts是一维数组 pts[0]到pts[3]代表Vec2i
 void triangle(Vec2i* pts, TGAImage& image, TGAColor color) 
 {
     Vec2i bboxmin(image.get_width() - 1, image.get_height() - 1);
@@ -126,6 +151,49 @@ void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage& image, TGAColor color)
     }
 }
 
+/// <summary>
+/// 三维模型，通过包围盒、判断zbuffer方式深度检测不画(image.set)深度失败的像素
+/// </summary>
+/// <param name="pts">带z的Vec3f类型 一维数组指针</param>
+/// <param name="zbuffer">float类型 一维数组指针</param>
+void triangle(Vec3f* pts, float* zbuffer,TGAImage& image,TGAColor color)
+{
+    Vec2f bboxmin(std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
+    Vec2f bboxmax(-std::numeric_limits<float>::max(), -std::numeric_limits<float>::max());
+    Vec2f clamp(image.get_width() - 1, image.get_height() - 1);
+    for (int i = 0; i < 3; i++)
+    {
+        //看里面就行，外面一层是保护不出界
+        bboxmin.x = std::max(0.0f, std::min(bboxmin.x, pts[i].x));
+        bboxmin.y = std::max(0.0f, std::min(bboxmin.y, pts[i].y));
+        bboxmax.x = std::min(clamp.x, std::max(bboxmax.x, pts[i].x));
+        bboxmax.y = std::min(clamp.y, std::max(bboxmax.y, pts[i].y));
+    }
+
+    Vec3f P;
+    for (P.x = bboxmin.x; P.x <= bboxmax.x; P.x++)
+    {
+        for (P.y = bboxmin.y; P.y <= bboxmax.y; P.y++)
+        {
+            Vec3f bc_screen = barycentric(pts, P);//bc_screen是P点的重心坐标
+            if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0)
+                continue;
+            P.z = 0;
+            P.z += pts[0].z * bc_screen.x;
+            P.z += pts[1].z * bc_screen.y;
+            P.z += pts[2].z * bc_screen.z;//这个是P点的z值
+            //for (int i = 0; i < 3; i++)
+            //    P.z += pts[i].z * bc_screen[i];//这个是P点的z值
+
+            if (zbuffer[int(P.x + P.y * width)] < P.z)
+            {
+                zbuffer[int(P.x + P.y * width)] = P.z;
+                image.set(P.x, P.y, color);
+            }
+        }
+    }
+}
+
 void rasterize(Vec2i p0, Vec2i p1, TGAImage& image, TGAColor color, int ybuffer[])
 {
     if (p0.x > p1.x)
@@ -144,43 +212,13 @@ void rasterize(Vec2i p0, Vec2i p1, TGAImage& image, TGAColor color, int ybuffer[
     }
 }
 
+Vec3f world2screen(Vec3f v)
+{
+    return Vec3f(int((v.x + 1.0) * width / 2.0 + 0.5), int((v.y + 1.0) * height / 2.0 + 0.5), v.z);
+}
+
 int main(int argc, char** argv) 
 {
-    {
-        TGAImage render(width, 16, TGAImage::RGB);
-        int ybuffer[width];
-        for (int i = 0; i < width; i++)
-        {
-            ybuffer[i] = std::numeric_limits<int>::min();
-        }
-        rasterize(Vec2i(20, 34), Vec2i(744, 400), render, red, ybuffer);
-        //rasterize(Vec2i(120, 434), Vec2i(444, 400), render, green, ybuffer);
-        //rasterize(Vec2i(330, 463), Vec2i(594, 200), render, blue, ybuffer);
-
-        // 1-pixel wide image is bad for eyes, lets widen it
-        for (int i = 0; i < width; i++)
-            for (int j = 1; j < 16; j++)
-                render.set(i, j, render.get(i, 0));
-
-        render.flip_vertically();
-        render.write_tga_file("render.tga");
-    }
-    return 1;
-
-    {
-        TGAImage scene(width, height, TGAImage::RGB);
-
-        line(Vec2i(20, 34), Vec2i(744, 400), scene, red);
-        line(Vec2i(120, 434), Vec2i(444, 400), scene, green);
-        line(Vec2i(330, 463), Vec2i(594, 200), scene, blue);
-
-        line(Vec2i(10, 10), Vec2i(790, 10), scene, white);
-
-        scene.flip_vertically();
-        scene.write_tga_file("scene.tga");
-    }
-    return 1;
-
     if (2 == argc)
     {
         model = new Model(argv[1]);
@@ -189,24 +227,34 @@ int main(int argc, char** argv)
     {
         model = new Model("obj/african_head.obj");
     }
+
+    float* zbuffer = new float[width * height];
+    for (int i = width * height; i--; zbuffer[i] = -std::numeric_limits<float>::max());//i减到0就为false了
+
     TGAImage image(width, height, TGAImage::RGB);
     Vec3f light_dir(0, 0, -1);
     for (int i = 0; i < model->nfaces(); i++)
     {
         std::vector<int> face = model->face(i);
-        Vec2i screen_coords[3];
-        Vec3f world_coords[3];
-        for (int j = 0; j < 3; j++)
-        {
-            Vec3f v = model->vert(face[j]);
-            screen_coords[j] = Vec2i((v.x + 1.) * width / 2., (v.y + 1.) * height / 2.);
-            world_coords[j] = v;
-        }
-        Vec3f normal = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);
-        normal.normalize();
-        float intensity = normal * light_dir;
-        if (intensity > 0)
-            triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+        Vec3f pts[3];
+        for (int i = 0; i < 3; i++)
+            pts[i] = world2screen(model->vert(face[i]));
+        triangle(pts, zbuffer, image, TGAColor(rand() % 255, rand() % 255, rand() % 255, 255));
+
+        //Vec2i screen_coords[3];
+        //Vec3f world_coords[3];
+        //for (int j = 0; j < 3; j++)
+        //{
+        //    Vec3f v = model->vert(face[j]);
+        //    screen_coords[j] = Vec2i((v.x + 1.) * width / 2., (v.y + 1.) * height / 2.);
+        //    world_coords[j] = v;
+        //}
+        //Vec3f normal = (world_coords[2] - world_coords[0]) ^ (world_coords[1] - world_coords[0]);
+        //normal.normalize();
+        //float intensity = normal * light_dir;
+        //if (intensity > 0)
+        //    triangle(screen_coords[0], screen_coords[1], screen_coords[2], image, TGAColor(intensity * 255, intensity * 255, intensity * 255, 255));
+
     }
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
